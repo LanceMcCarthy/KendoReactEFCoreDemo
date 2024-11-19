c5bfeac5423b91f60e037a9c18e0694d94e7e89e
Hetali Shah Gala <hetali.shah.gala@gmail.com>
2024-11-19 14:25:59 -0500

    Added Kendo Grid

--- src/reactwithrestapi.client/src/App.jsx
+++ src/reactwithrestapi.client/src/App.jsx
@@ -1,51 +1,21 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 
-function App() {
-    const [forecasts, setForecasts] = useState();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+const App = () => {
+    const [customers, setCustomers] = useState([]);
+        useEffect(() => {
+            fetch('https://localhost:7241/api/customers')
+                .then((response) => response.json())
+                .then(setCustomers);
+        }, []);
 
     return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
+        <Grid
+            data={customers}
+        >
+            <Column field="firstName" title="Name" />
+        </Grid>
     );
-    
-    async function populateWeatherData() {
-        const response = await fetch('https://localhost:7241/api/weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
-    }
 }
 
 export default App;
