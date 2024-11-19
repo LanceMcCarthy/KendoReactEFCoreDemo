9782847d319fb2f459371b354de15d18dc2ad409
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-11-19 15:09:56 -0500

    Updated Grid to use kendo-data-query library

--- src/reactwithrestapi.client/src/App.jsx
+++ src/reactwithrestapi.client/src/App.jsx
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 
+import { withState } from './GridWithState.jsx';
+
+const StatefullGrid = withState(Grid);
+
 const App = () => {
-    const [customers, setCustomers] = useState([]);
-        useEffect(() => {
-            fetch('https://localhost:7241/api/customers')
-                .then((response) => response.json())
-                .then(setCustomers);
-        }, []);
+    //const [customers, setCustomers] = useState([]);
+    //useEffect(() => {
+    //    fetch('https://localhost:7241/api/customers')
+    //        .then((response) => response.json())
+    //        .then(setCustomers);
+    //}, []);
 
     return (
-        <Grid
-            data={customers}
-        >
-            <Column field="firstName" title="Name" />
-        </Grid>
+        <div>
+            <StatefullGrid>
+                <Column field="firstName" title="First" />
+                <Column field="lastName" title="Last" />
+                <Column field="phone" title="Phone" />
+                <Column field="email" title="Email" />
+                <Column field="street" title="street" />
+                <Column field="city" title="City" />
+                <Column field="state" title="State" />
+                <Column field="zipCode" title="ZIP Code" />
+            </StatefullGrid>
+        </div>
     );
 }
 
