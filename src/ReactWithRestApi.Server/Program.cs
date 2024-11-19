7233e2dae62b9c233cca735ed0b89ca6841bf767
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-11-19 12:52:57 -0500

    Add EntityFramework and live SQL connection.

--- src/ReactWithRestApi.Server/Program.cs
+++ src/ReactWithRestApi.Server/Program.cs
@@ -1,14 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using ReactWithRestApi.Server.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// CORS is set to allow any origin to access the web API.
+// If your front end is on a domain, you will need to change this to the domain of your front end so that only requests form your domain can use the REST API
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "_myAllowAllOrigins", policy  => { policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); });
 });
 
+
+// NOTE: this uses the connection string from appsettings.json
+builder.Services.AddDbContext<LanceDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LanceSqlServer"));
+});
+
+
+// This surfaces the API controller
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// These gives oyu a nice swagger UI when you want to see the REST endpoints at /swagger/index.html
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +30,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
